db89198a16317008a6da6a22fde1e60c59b11a08
Saqib Nawab <saqibnawab1051@gmail.com>
2024-12-31 13:47:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let num = 1234
+let age = "Thirty two"
+let roll = null
+let vvv = true
+// alert("Saqib ")
+/*
+
+number -> 2 - 1*58
+string -> " saqib"
+boolean = true/false
+bigint ->
+null -> standalone value
+undefined -> let name;
+
+
+*/
+console.log(typeof age);
+console.log(typeof num);
+console.log(typeof roll);
+console.log(typeof vvv);
+console.log(typeof undefined);
