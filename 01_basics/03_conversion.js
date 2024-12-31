aeeb7560ff531127e24d24f8b446b8fad883dcca
Saqib Nawab <saqibnawab1051@gmail.com>
2024-12-31 14:35:55 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,28 @@
+// let score = null
+
+// console.log(typeof score);
+// console.log(score);
+
+
+// let name = Number(score);
+// console.log(typeof name);
+// console.log(name);
+
+/*
+33 = 33
+33abc = NaN
+true = 1, false = 0
+null = 0
+*/
+// let value = 101
+// let booleanvalue = Boolean(value)
+// console.log(booleanvalue);
+
+// 1 = true, 0= false;
+// "" = false, " sa12" = true;
+
+let somenumber = 33
+let some = String(somenumber);
+console.log(some);
+console.log(typeof some);
+
