997ecdb1bbd4dc1a3eec9c48635d2c63319fcd2e
Saqib Nawab <saqibnawab1051@gmail.com>
2024-12-31 13:50:31 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountName = "Saqib nawab"
+var accountEmail = "Saqibnawab@.com"
+accountCity = "Chunian"
+let accountState;
+// accounthome;
+
+// accountId = 2222
+console.log(accountId);
+console.table([accountId, accountName, accountEmail, accountCity, accountState,])
