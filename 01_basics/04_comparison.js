4f5b36f1c20d51df80a4c9aaa2c1ca69875d7d08
Saqib Nawab <saqibnawab1051@gmail.com>
2025-01-01 14:11:39 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,52 @@
+// console.log(2 < 1 );
+// console.log(2 <= 1 );
+// console.log(2 == 1 );
+// console.log(2 > 1 );
+// console.log(2 >= 1 );
+// console.log(2 != 1 );
+
+// console.log("2" < 1 );
+// console.log("2" <= 1 );
+// console.log("2" == "1" );
+// console.log("2" > 1 );
+// console.log("2" >= 1 );
+// console.log("2" != 1 );
+
+// console.log(  null > 0);
+// console.log(  null >= 0);
+// console.log(  null == 0);
+// console.log(  null < 0);
+// console.log(  null <= 0);
+// console.log(  null != 0);
+
+// console.log(  undefined >  0   )  ;
+// console.log(  undefined >=     0  );
+// console.log(  undefined ==     0  );
+// console.log(  undefined <  0   )  ;
+// console.log(  undefined <=     0  );
+// console.log(  undefined !=     0  );
+
+// console.log(  undefined >      null   )  ;
+// console.log(  undefined >=     null  );
+// console.log(  undefined ==     null  );
+// console.log(  undefined <      null   )  ;
+// console.log(  undefined <=     null  );
+// console.log(  undefined !=     null  );
+
+// console.log( "0" == null);
+// console.log( "0" === null);
+
+let id = Symbol('1234');
+let smallid = Symbol('1234');
+// console.log(id);
+// console.log(smallid);
+// console.log(id != smallid);
+
+let bignumber = 9*999999999999999999999n
+console.log(typeof bignumber);
+
+
+
+
+
+
