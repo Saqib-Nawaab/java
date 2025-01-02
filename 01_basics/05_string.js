eb28cf157c60b8a1f6f75575a0b65e21a695f77b
Saqib Nawab <saqibnawab1051@gmail.com>
2025-01-02 15:25:04 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,46 @@
+let name = "             Saqib nawab           "
+let bb = 'saqib'
+let = 22
+console.log(`Hello my name is ${bb} is and my age is `);
+
+
+// console.log(name.trim());
+// console.log(name);
+// console.log(name.trim());
+// console.log(name);
+// let score = 'Helilo my niame is Saqib nawab.i study Bscs in UE'
+// console.log(score.replaceAll('i', ''));
+// console.log(score.includes('name', 'Saqib'));
+// console.log(score.includes('niame','opp'));
+
+
+
+let nnn = 'Saqib - nawab - is - a - good - person '
+// console.log(nnn.split(' ', ' -'));
+
+// const getname = new String('kingbossbaby')
+// const tetname = new String('kingbossbaby')
+
+// let newname = tetname.slice(0, 5)
+// let gewname = tetname.slice(-10, -6)
+
+// console.log(newname);
+// console.log(gewname);
+
+
+
+// let newname = tetname.substring(3, 8)
+// console.log(name);
+// console.log(getname);
+// console.log(getname[3]);
+// console.log(name.length);
+// console.log(name.__proto__);
+// console.log(getname.toLowerCase());
+// console.log(getname.indexOf('y'));
+// console.log(newname);
+
+
+// console.log(getname.charAt(5));
+// console.log(name.indexOf('b'));
+
+
