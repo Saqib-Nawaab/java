70bc90f8ac477346bcfa29af50dc4f3d6955b0b4
Saqib Nawab <saqibnawab1051@gmail.com>
2025-01-02 16:40:47 +0000

    nums

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,13 @@
+// let score = 9999
+// let name = new Number(8888)
+// let age = 11123.8999454
+// let age = 1767657600000000000000000099
+// console.log(typeof name);
+// console.log( name);
+// console.log( name.toString().length);
+// console.log( name.toFixed(6));
+// console.log( name.__proto__);
+// console.log(age.toPrecision(3));
+// console.log(age.toLocaleString());
+
+
