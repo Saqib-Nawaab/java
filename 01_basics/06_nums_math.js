14237b1682e94a9ce68d061ff85e521713daa4e1
Saqib Nawab <saqibnawab1051@gmail.com>
2025-01-03 05:38:04 +0000

    nms and math

--- 01_basics/06_nums_math.js
+++ 01_basics/06_nums_math.js
@@ -1,13 +1,45 @@
 // let score = 9999
 // let name = new Number(8888)
 // let age = 11123.8999454
-// let age = 1767657600000000000000000099
+let age = 1000000000000000000099
 // console.log(typeof name);
 // console.log( name);
 // console.log( name.toString().length);
 // console.log( name.toFixed(6));
 // console.log( name.__proto__);
 // console.log(age.toPrecision(3));
-// console.log(age.toLocaleString());
+// console.log(age.toLocaleString('en-PK'));
 
+let score = [12,23,45,2+9,3-99,2-9,45,56,67,78-1, 79-2,77+9];
+let maxi = Math.min(...score);
+// console.log(maxi);
+// console.log(Math.abs(maxi));
+// console.log(Math.round(9.5));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.1));
+// console.log(Math.pow(3, 2));
+// console.log(Math.pow(2, 3));
+// console.log(Math.min(2,3,44,22,1,22,333,555,6676,3223));
+// console.log(Math.max(2,3,44,22,1,22,333,555,6676,3223));
+
+console.log(Math.random());
+// console.log(Math.floor(Math.random()*10));
+// console.log(Math.floor(Math.random()*10));
+// console.log(Math.floor(Math.random()*10));
+// console.log(Math.floor(Math.random()*10));
+// console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*(20-16)+16));
+console.log(Math.floor(Math.random()*(20-16)+16));
+console.log(Math.floor(Math.random()*(20-16)+16));
+console.log(Math.floor(Math.random()*(20-16)+16));
+console.log(Math.floor(Math.random()*(20-16)+16));
+// console.log(Math.floor(Math.random()*(20-10+1))+10);
+// console.log(Math.floor(Math.random()*(20-10+1)+10));
+// console.log(Math.floor(Math.random()*(20-10+1))+10);
+// console.log(Math.floor(Math.random()*(20-10+1)+10));
+// console.log(Math.floor(Math.random()*(20-10+1))+10);
+// console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
 
