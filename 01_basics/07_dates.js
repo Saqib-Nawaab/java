2f0ef53defbc8c3c66cde5d0ecd863ab31631225
Saqib Nawab <saqibnawab1051@gmail.com>
2025-01-03 06:14:15 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,20 @@
+// console.log(setTimeout);
+
+// let timee = 
+// console.log('Comlete ', Temporal.Now.instant());
+ let mydates = new Date("11-01-2022")
+  console.log( mydates.toDateString());
+  console.log( mydates.toLocaleString());
+
+
+
+
+//  console.log(typeof mydates);
+//  console.log(mydates.toString());
+//  console.log(mydates.toISOString());
+//  console.log(mydates.toLocaleString());
+//  console.log(mydates.toLocaleTimeString());
+//  console.log(mydates.toJSON());
+//  console.log(mydates.toLocaleDateString());
+//  console.log(mydates.toDateString());
+ 
