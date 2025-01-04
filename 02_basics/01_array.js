242d245d99d85252c8c548af2de3a0ff853107be
Saqib Nawab <saqibnawab1051@gmail.com>
2025-01-04 06:45:34 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,46 @@
+// const arr = [0,1,2,3,4,5]
+
+// const heroes =  arr[0] + 2
+// console.log(heroes);
+// console.log(arr+3);
+//  arr.push(arr[5])
+//  console.log(arr);
+ 
+// const name = [0,1,2,3,4,5]
+// console.log('A ' , name);
+// // console.log(myname);
+// const myname = name.slice(1,3)
+// console.log(myname);
+// console.log('A ' , name);
+// console.log(myname);
+ 
+
+// const mname = name.splice(1,3)
+// console.log('B ' , name);
+// console.log(mname);
+// console.log('B ' , name);
+// console.log(mname);
+// console.log('B ' , name);
+
+// let name = ['saqib',"Nawab","good"];
+// let myname = ["college","school","Home"];
+// let fff = name.concat(myname);
+// console.log(fff);
+// console.log(typeof fff);
+// let nnn = [...name,...myname];
+// console.log(nnn);
+// console.log(typeof nnn);
+
+
+// name.push(myname)
+// console.log(name);
+// console.log(typeof name);
+
+// let name = [1,2,[3,4,5],6,7,[,8,9,10],11,[12,13,[14,15,16,[17,18,19,20,[21],22,[23,24,25,26,27],],],],];
+// let real = name.flat(Infinity);
+// console.log(real);
+
+let name = 'saqib nawab';
+console.log(Array.isArray(name));
+console.log(Array.from(name));
+console.log(Array.isArray(name));
