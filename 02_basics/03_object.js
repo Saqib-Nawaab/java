2626c92ffbae3957ed9458e1a303ffa8df11be1f
Saqib Nawab <saqibnawab1051@gmail.com>
2025-01-08 05:54:57 +0000

    object

---
+++ 02_basics/03_object.js
@@ -0,0 +1,23 @@
+
+let chatgpt = {}
+
+chatgpt.id = "aqib asnawab";
+chatgpt.age = 22;
+chatgpt.login = false
+
+// console.log(chatgpt);
+let data = {
+    email : "saqibnawab@.com",
+    fullname:{
+        userone:{
+            name : "Saqib nawab",
+            mynumber : "03041437740",
+            // idloginofuser : false
+
+        }
+    }
+}
+console.log(data.fullname);
+// console.log("hello world");
+
+
