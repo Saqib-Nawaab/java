c28e2fb6d8919a71d0742907a2c9f2041364965c
Saqib Nawab <saqibnawab1051@gmail.com>
2025-01-08 06:59:18 +0000

    objects

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -3,21 +3,51 @@ let chatgpt = {}
 
 chatgpt.id = "aqib asnawab";
 chatgpt.age = 22;
-chatgpt.login = false
+chatgpt.login = false;
 
 // console.log(chatgpt);
-let data = {
-    email : "saqibnawab@.com",
-    fullname:{
-        userone:{
-            name : "Saqib nawab",
-            mynumber : "03041437740",
-            // idloginofuser : false
+// let data = {
+//     email : "saqibnawab@.com",
+//     fruit : "Mango",
+//     fullname:{
+//         logo : "SSS",
+//         userone:{
+//             name : "Saqib nawab",
+//             mynumber : "03041437740",
+//             // idloginofuser : false
 
-        }
-    }
-}
-console.log(data.fullname);
+//         }
+//     }
+// }
+// console.log(data.fullname);
+// console.log(data.fullname.userone.name);
+
+
+
 // console.log("hello world");
 
+let object1 = {1: "a", 2: "b"};
+let object2 = {3: "a1", 2: "b2"};
+// let object3 = {object1, object2};
+// let object3 = Object.assign(object1,object2)
+// let = object3 = {...object1,...object2}
+// console.log(object3);
+ let user = [
+    {
+        memes : "Haaaaa",
+        mike : "@@@@@",
+    },
+    {
+
+    },
+    {
+
+    }, 
+]
+
+// console.log(chatgpt);
+// console.log(Object.keys(chatgpt));
+// console.log(Object.values(chatgpt));
+// console.log(Object.entries(chatgpt));
+console.log(chatgpt.hasOwnProperty('logn'));
 
