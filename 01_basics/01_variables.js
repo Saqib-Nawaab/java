20fa3fbbc6a58679db004c11b8eba744717875e3
Saqib Nawab <saqibnawab1051@gmail.com>
2025-01-08 05:55:34 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,5 +6,30 @@ let accountState;
 // accounthome;
 
 // accountId = 2222
-console.log(accountId);
-console.table([accountId, accountName, accountEmail, accountCity, accountState,])
+// console.log(accountId);
+// console.table([accountId, accountName, accountEmail, accountCity, accountState,])
+// {
+//     // let name = "saqib";
+// }{
+
+//     // let name = aliasasad;
+// }
+
+//     console.log(12);
+// const browserType = "zillajk";
+
+// if (browserType.startsWith("zilla")) {
+//   console.log("Found zilla!");
+// } else {
+//   console.log("No zilla here!");
+// }
+// const tagline = "MDN-developers, by developers";
+// console.log(tagline.indexOf("developers")); // 20
+// console.log(tagline.indexOf("x")); // -1
+
+let quote = "To be or not to be";
+quote = quote.replace("be", "code");
+
+console.log(quote); // "To code or not to code"
+
+
